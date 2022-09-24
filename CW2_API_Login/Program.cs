ea97b0058d9d8b4a1b804751138b658dce8da95a
Pedro Itaboray <pedroh_itaboray@hotmail.com>
2022-09-24 13:48:38 -0300

    Atualização

--- CW2_API_Login/Program.cs
+++ CW2_API_Login/Program.cs
@@ -1,43 +1,81 @@
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+builder.Services.AddDbContext<PrestadorDb>(opt => opt.UseInMemoryDatabase("UsuarioList"));
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.MapGet("/prestadores", async (PrestadorDb db) =>
+    await db.Usuarios.Select(x => new PrestadorDTO(x)).ToListAsync());
 
-app.UseHttpsRedirection();
+app.MapGet("/prestadores/{id}", async (int id, PrestadorDb db) =>
+    await db.Usuarios.FindAsync(id)
+        is Prestador usuario
+            ? Results.Ok(new PrestadorDTO(usuario))
+            : Results.NotFound());
 
-var summaries = new[]
+app.MapPost("/prestadores", async (Prestador usuario, PrestadorDb db) =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+    db.Usuarios.Add(usuario);
+    await db.SaveChangesAsync();
 
-app.MapGet("/weatherforecast", () =>
+    return Results.Created($"/prestadores/{usuario.Id}", new PrestadorDTO(usuario));
+});
+
+app.MapPut("/prestadores/{id}", async (int id, PrestadorDTO usuarioDTO, PrestadorDb db) =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+    var usuario = await db.Usuarios.FindAsync(id);
+
+    if (usuario is null) return Results.NotFound();
+
+    usuario.Nome = usuarioDTO.Nome;
+    usuario.Ativo = usuarioDTO.Ativo;
+
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+
+app.MapDelete("/prestadores/{id}", async (int id, PrestadorDb db) =>
+{
+    if (await db.Usuarios.FindAsync(id) is Prestador usuario)
+    {
+        db.Usuarios.Remove(usuario);
+        await db.SaveChangesAsync();
+        return Results.Ok(new PrestadorDTO(usuario));
+    }
+
+    return Results.NotFound();
+});
 
 app.Run();
 
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+public class Prestador
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    public int Id { get; set; }
+    public string? Nome { get; set; }
+    public string? cgccpf { get; set; }
+    public bool Ativo { get; set; }
+}
+
+public class PrestadorDTO
+{
+    public int Id { get; set; }
+    public string? Nome { get; set; }
+    public string? cgccpf { get; set; }
+    public bool Ativo { get; set; }
+
+    public PrestadorDTO() { }
+    public PrestadorDTO(Prestador usuario) =>
+    (Id, Nome, Ativo, cgccpf) = (usuario.Id, usuario.Nome, usuario.Ativo, usuario.cgccpf);
+}
+
+
+class PrestadorDb : DbContext
+{
+    public PrestadorDb(DbContextOptions<PrestadorDb> options)
+    : base(options) { }
+
+    public DbSet<Prestador> Usuarios => Set<Prestador>();
 }
